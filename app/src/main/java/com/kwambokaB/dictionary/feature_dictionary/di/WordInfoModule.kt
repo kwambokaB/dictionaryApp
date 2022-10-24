0a1ff5ded4c7d200c81e7a217c0d4a30becf733b
kwambokaB <paula.paulatek@gmail.com>
2022-10-24 17:37:44 +0300

    implemented the dagger hilt configuration

---
+++ app/src/main/java/com/kwambokaB/dictionary/feature_dictionary/di/WordInfoModule.kt
@@ -0,0 +1,53 @@
+package com.kwambokaB.dictionary.feature_dictionary.di
+
+import android.app.Application
+import androidx.room.Room
+import com.google.gson.Gson
+import com.kwambokaB.dictionary.feature_dictionary.data.local.WordInfoDB
+import com.kwambokaB.dictionary.feature_dictionary.data.local.WordInfoDao
+import com.kwambokaB.dictionary.feature_dictionary.data.remote.DictionaryApi
+import com.kwambokaB.dictionary.feature_dictionary.data.repository.WordInfoRepositoryImpl
+import com.kwambokaB.dictionary.feature_dictionary.data.util.GsonParser
+import com.kwambokaB.dictionary.feature_dictionary.domain.repository.WordInfoRepository
+import com.kwambokaB.dictionary.feature_dictionary.domain.use_case.GetWordInfo
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object WordInfoModule {
+@Provides
+@Singleton
+fun provideGetWordInfoUseCase(repository: WordInfoRepository): GetWordInfo{
+    return GetWordInfo(repository)
+}
+    @Provides
+    @Singleton
+    fun provideWordInfoRepository(dao: WordInfoDao, api: DictionaryApi): WordInfoRepository{
+        return WordInfoRepositoryImpl(api, dao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDataBase(app:Application): WordInfoDB{
+        return  Room.databaseBuilder(
+            app, WordInfoDB::class.java, "word_db"
+        ).addTypeConverter(GsonParser(Gson()))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDictionaryApi() : DictionaryApi{
+        return Retrofit.Builder()
+            .baseUrl(DictionaryApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(DictionaryApi::class.java)
+    }
+}
